038aa3dfdd08b52160e9b02e390c67cad4bdbe5f
AndreyEfimenko <andrey.efimenko.sp@gmail.com>
2022-04-05 21:13:15 +0300

    feat: add solution for two tasks

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,125 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+	for (let i = 0; i < matrix.length; i++) {
+		const element = matrix[i];
+		element.forEach((elem, index) => elem === false ? element[index] = 0 : true)
+	}
+	for (let j = 0; j < matrix.length; j++) {
+		const el = matrix[j];
+		const position = el.indexOf(true);
+		// 1-ый ряд!
+		if (j === 0 && position === 0) {
+			if (matrix[j+1][position] === true) {
+			} else matrix[j+1][position] = matrix[j+1][position] + 1;
+			if (matrix[j][position+1] === true) {
+			} else matrix[j][position+1] = matrix[j][position+1] + 1;
+			if (matrix[j+1][position+1] === true) {
+				continue;
+			} else matrix[j+1][position+1] = matrix[j+1][position+1] + 1;
+		}
+		if (j === 0 && position === 1) {
+			if (matrix[j][position-1] === true) {
+			} else matrix[j][position-1] = matrix[j][position-1] + 1;
+			if (matrix[j][position+1] === true) {
+			} else matrix[j][position+1] = matrix[j][position+1] + 1;
+			if (matrix[j+1][position+1] === true) {
+			} else matrix[j+1][position+1] = matrix[j+1][position+1] + 1;
+			if (matrix[j+1][position-1] === true) {
+			} else matrix[j+1][position-1] = matrix[j+1][position-1] + 1;
+			if (matrix[j+1][position] === true) {
+			} else matrix[j+1][position] = matrix[j+1][position] + 1;
+		}
+		if (j === 0 && position === 2) {
+			if (matrix[j+1][position] === true) {
+			} else matrix[j+1][position] = matrix[j+1][position] + 1;
+			if (matrix[j][position-1] === true) {
+			} else matrix[j][position-1] = matrix[j][position-1] + 1;
+			if (matrix[j+1][position-1] === true) {
+				continue;
+			} else matrix[j+1][position-1] = matrix[j+1][position-1] + 1;
+		}
+		//2-ой ряд!
+		if (j === 1 && matrix.length >= 3 && position === 0) {
+			if (matrix[j+1][position] === true) {
+			} else matrix[j+1][position] = matrix[j+1][position] + 1;
+			if (matrix[j-1][position] === true) {
+			} else matrix[j-1][position] = matrix[j-1][position] + 1;
+			if (matrix[j-1][position+1] === true) {
+			} else matrix[j-1][position+1] = matrix[j-1][position+1] + 1;
+			if (matrix[j][position+1] === true) {
+			} else matrix[j][position+1] = matrix[j][position+1] + 1;
+			if (matrix[j+1][position+1] === true) {
+			} else matrix[j+1][position+1] = matrix[j+1][position+1] + 1;
+		}
+		if (j === 1 && matrix.length >= 3 && position === 1) {
+			if (matrix[j+1][position] === true) {
+			} else matrix[j+1][position] = matrix[j+1][position] + 1;
+			if (matrix[j-1][position] === true) {
+			} else matrix[j-1][position] = matrix[j-1][position] + 1;
+			if (matrix[j-1][position+1] === true) {
+			} else matrix[j-1][position+1] = matrix[j-1][position+1] + 1;
+			if (matrix[j][position+1] === true) {
+			} else matrix[j][position+1] = matrix[j][position+1] + 1;
+			if (matrix[j+1][position+1] === true) {
+			} else matrix[j+1][position+1] = matrix[j+1][position+1] + 1;
+			if (matrix[j+1][position-1] === true) {
+			} else matrix[j+1][position-1] = matrix[j+1][position-1] + 1;
+			if (matrix[j][position-1] === true) {
+			} else matrix[j][position-1] = matrix[j][position-1] + 1;
+			if (matrix[j-1][position-1] === true) {
+			} else matrix[j-1][position-1] = matrix[j-1][position-1] + 1;
+		}
+		if (j === 1 && matrix.length >= 3 && position === 2) {
+			if (matrix[j+1][position] === true) {
+			} else matrix[j+1][position] = matrix[j+1][position] + 1;
+			if (matrix[j-1][position] === true) {
+			} else matrix[j-1][position] = matrix[j-1][position] + 1;
+			if (matrix[j-1][position-1] === true) {
+			} else matrix[j-1][position-1] = matrix[j-1][position-1] + 1;
+			if (matrix[j][position-1] === true) {
+			} else matrix[j][position-1] = matrix[j][position-1] + 1;
+			if (matrix[j+1][position-1] === true) {
+			} else matrix[j+1][position-1] = matrix[j+1][position-1] + 1;
+		}
+		//3-ий ряд!
+		if (j === 2 && matrix.length >= 3 && position === 0) {
+			if (matrix[j-1][position] === true) {
+			} else matrix[j-1][position] = matrix[j-1][position] + 1;
+			if (matrix[j][position+1] === true) {
+			} else matrix[j][position+1] = matrix[j][position+1] + 1;
+			if (matrix[j-1][position+1] === true) {
+				continue;
+			} else matrix[j-1][position+1] = matrix[j-1][position+1] + 1;
+		}
+		if (j === 2 && position === 1) {
+			if (matrix[j][position-1] === true) {
+			} else matrix[j][position-1] = matrix[j][position-1] + 1;
+			if (matrix[j][position+1] === true) {
+			} else matrix[j][position+1] = matrix[j][position+1] + 1;
+			if (matrix[j-1][position+1] === true) {
+			} else matrix[j-1][position+1] = matrix[j-1][position+1] + 1;
+			if (matrix[j-1][position] === true) {
+			} else matrix[j-1][position] = matrix[j-1][position] + 1;
+			if (matrix[j-1][position-1] === true) {
+			} else matrix[j-1][position-1] = matrix[j-1][position-1] + 1;
+		}
+		if (j === 2 && position === 2) {
+			if (matrix[j-1][position] === true) {
+			} else matrix[j-1][position] = matrix[j-1][position] + 1;
+			if (matrix[j][position-1] === true) {
+			} else matrix[j][position-1] = matrix[j][position-1] + 1;
+			if (matrix[j-1][position-1] === true) {
+				continue;
+			} else matrix[j-1][position-1] = matrix[j-1][position-1] + 1;
+		}
+	}
+	for (let i = 0; i < matrix.length; i++) {
+		const element = matrix[i];
+		element.forEach((elem, index) => elem === true ? element[index] = 1 : true)
+	}
+	return matrix;
 }
 
 module.exports = {
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,27 +4,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+ const chainMaker = {
+	stack: [],
+	getLength() {
+		this.stack.length;
+		return this;
+	},
+	addLink(value) {
+		if (value === undefined) {
+			this.stack.push(`(  )`);
+		} else this.stack.push(`~~( ${value} )`);
+		return this;
+	},
+	removeLink(position) {
+		if (typeof position !== 'number' || position === 0 || position < 0 || !(Number.isInteger(position)) || position > this.stack.length - 1) {
+			this.stack = [];
+			throw new Error(`You can't remove incorrect link!`)
+		} else {
+			this.stack.splice(position - 1, 1);
+			return this;
+		}
+	},
+	reverseChain() {
+		this.stack.reverse();
+		return this;
+	},
+	finishChain() {
+		let a = this.stack.join('').substring(2)
+		this.stack = [];
+		return a;
+	}
 };
 
 module.exports = {
