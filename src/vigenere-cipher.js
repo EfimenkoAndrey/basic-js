79f131b4d1f3bb3cd68c8549adb0ec976fca8fcc
AndreyEfimenko <andrey.efimenko.sp@gmail.com>
2022-04-29 16:08:34 +0300

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,87 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor(flag = true) {
+		this.reverseflag = flag;
+	}
+	encrypt(string, key) {
+		if (string === undefined || key === undefined ) {
+			throw new Error('Incorrect arguments!')
+		}
+		let workStr = string.toUpperCase();
+		let azbuka = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		let shifr = [];
+		let stackShifr = key.split('');
+		let result = [];
+		for (let i = 0; i < string.length; i++) {
+			if (string[i] === ' ') {
+				shifr.push(' ');
+				stackShifr.unshift('!');
+			} else {
+				if (stackShifr[i] === undefined) {
+					stackShifr = key.split('');
+					string = string.slice(i);
+					i = 0;
+				}
+				shifr.push(stackShifr[i].toUpperCase());
+			}
+		}
+		for(let i = 0; i < workStr.length; i++) {
+			let index = 0;
+			if (azbuka.indexOf(workStr[i]) > -1) {
+				if ((azbuka.indexOf(workStr[i]) + azbuka.indexOf(shifr[i])) > azbuka.length - 1) {
+					index = Math.abs(azbuka.length - (azbuka.indexOf(workStr[i]) + azbuka.indexOf(shifr[i])));
+				} else {
+					index = azbuka.indexOf(workStr[i]) + azbuka.indexOf(shifr[i]);
+				}
+				result.push(azbuka[index]);
+			} else {
+				result.push(workStr[i]);
+			}
+		}
+		if (this.reverseflag === false) {
+			return result.reverse().join('');
+		} else return result.join('');
+	}
+	decrypt(string, key) {
+		if (string === undefined || key === undefined ) {
+			throw new Error('Incorrect arguments!')
+		}
+		let workStr = string.toUpperCase();
+		let azbuka = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		let shifr = [];
+		let stackShifr = key.split('');
+		let result = [];
+		for (let i = 0; i < string.length; i++) {
+			if (string[i] === ' ') {
+				shifr.push(' ');
+				stackShifr.unshift('!');
+			} else {
+				if (stackShifr[i] === undefined) {
+					stackShifr = key.split('');
+					string = string.slice(i);
+					i = 0;
+				}
+				shifr.push(stackShifr[i].toUpperCase());
+			}
+		}
+		for(let i = 0; i < workStr.length; i++) {
+			let index = 0;
+			if (azbuka.indexOf(workStr[i]) > -1) {
+				if ((azbuka.indexOf(workStr[i]) < azbuka.indexOf(shifr[i]))) {
+					index = Math.abs(azbuka.length + (azbuka.indexOf(workStr[i]) - azbuka.indexOf(shifr[i])));
+				} else {
+					index = azbuka.indexOf(workStr[i]) - azbuka.indexOf(shifr[i]);
+				}
+				result.push(azbuka[index]);
+			} else {
+				result.push(workStr[i]);
+			}
+		}
+		if (this.reverseflag === false) {
+			return result.reverse().join('');
+		} else return result.join('');
+	}
 }
 
 module.exports = {
